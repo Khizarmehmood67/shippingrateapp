325a733bdec7ca5ece3b878f1c1a459d8cfdd321
KhizarMehmood <khizar@kodxsystem.com>
2024-08-08 18:18:27 +0500

    new code updated

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -1,27 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import axios from 'axios';
 
 export async function POST(req) {
   const { SHOPIFY_API_SECRET, SHOPIFY_SHOP } = process.env;
 
   try {
-    // Parse the request body
     const body = await req.json();
 
-    console.log('Received body:', body);  // Log the entire incoming body for debugging
+    console.log('Received body:', body);
 
     if (body && body.order) {
       const { order } = body;
-      console.log('Received order:', order);  // Log incoming order for debugging
+      console.log('Received order:', order);
 
       const orderId = order.id;
-      const orderPrice = order.total_price / 100; // Convert from cents to euros
+      const orderPrice = order.total_price / 100;
 
       let shippingCost = 0;
 
       if (orderPrice < 10) {
-        const productPrice = order.line_items[0].price / 100; // Assuming single product for simplicity, convert to euros
-        shippingCost = productPrice * 0.2; // 20% of the product price
+        const productPrice = order.line_items[0].price / 100;
+        shippingCost = productPrice * 0.2;
       } else {
         console.log('Normal shipping options apply for order:', orderId);
         return NextResponse.json({ message: 'Normal shipping options apply' });
@@ -37,7 +36,7 @@ export async function POST(req) {
             shipping_lines: [
               {
                 title: 'Custom Shipping',
-                price: Math.round(shippingCost * 100), // Convert back to cents for Shopify
+                price: Math.round(shippingCost * 100),
                 code: 'CUSTOM_SHIPPING',
                 source: 'custom-shopify-app'
               }
@@ -64,8 +63,6 @@ export async function POST(req) {
   }
 }
 
-export const config = {
-  api: {
-    bodyParser: false, // Disabling Next.js body parsing
-  },
+export const metadata = {
+  bodyParser: false,
 };
